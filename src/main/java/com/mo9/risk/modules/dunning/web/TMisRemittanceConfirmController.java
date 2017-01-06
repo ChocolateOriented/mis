d8682f74caa0c6b332d7029c1c98d6e8933f197b
sxu <sxu@L-EOC-0036.mo9.com>
2017-01-06 20:24:12 +0800

    l

--- src/main/java/com/mo9/risk/modules/dunning/web/TMisRemittanceConfirmController.java
+++ src/main/java/com/mo9/risk/modules/dunning/web/TMisRemittanceConfirmController.java
@@ -62,7 +62,7 @@ public class TMisRemittanceConfirmController extends BaseController {
 
 	@Autowired
 	private TMisRemittanceConfirmService tMisRemittanceConfirmService;
-	
+	 
 	@ModelAttribute
 	public TMisRemittanceConfirm get(@RequestParam(required=false) String id) {
 		TMisRemittanceConfirm entity = null;
