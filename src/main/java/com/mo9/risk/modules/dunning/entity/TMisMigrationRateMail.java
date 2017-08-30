40efd75432c648f5827308f03922ecd4dae15f95
chijw <530455097@qq.com>
2017-08-30 17:52:24 +0800

    迁徙发送邮件

---
+++ src/main/java/com/mo9/risk/modules/dunning/entity/TMisMigrationRateMail.java
@@ -0,0 +1,44 @@
+package com.mo9.risk.modules.dunning.entity;
+
+import java.math.BigDecimal;
+import java.util.List;
+import com.thinkgem.jeesite.common.persistence.DataEntity;
+
+
+/**
+ * 迁徙图表实体类
+ * @author jwchi
+ *
+ */
+public class TMisMigrationRateMail  {
+
+	
+	private String name;
+	private List<BigDecimal> data;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public List<BigDecimal> getData() {
+		return data;
+	}
+
+
+
+	public void setData(List<BigDecimal> data) {
+		this.data = data;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+
+}
